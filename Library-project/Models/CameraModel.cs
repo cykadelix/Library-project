1d0419b1fe650116b02a93680c524160d4328f1c
Richard Phong <richard.phong424@gmail.com>
2023-03-17 17:34:41 -0500

    Added camera, computer, and projector models.

---
+++ Library-project/Models/CameraModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_project.Models
+{
+    public class CameraModel
+    {
+        [Key]
+        public int serialNumber { get; set; }
+
+        public string brand { get; set; }
+        public string description { get; set; }
+        public int lumens { get; set; }
+        public bool Availibility { get; set; }
+
+        public CameraModel()
+        {
+
+        }
+    }
+}
---
+++ Library-project/Models/ComputerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_project.Models
+{
+    public class ComputerModel
+    {
+        [Key]
+        public int serialNumber { get; set; }
+
+        public string brand { get; set; }
+        public string description { get; set; }
+        public bool Availibility { get; set; }
+
+        public ComputerModel()
+        {
+
+        }
+    }
+}
