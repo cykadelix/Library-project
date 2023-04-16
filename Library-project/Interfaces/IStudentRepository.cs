57ad25825a6ef4d1529b58624a11dd88b2af5193
Emmanuel Chavez <chavemm15@gmail.comchavemm@Emmanuels-Air ~ % git config --global user.email chavemm15@gmailcomgit config --list>
2023-04-16 01:18:05 -0500

    Added repository pattern for student, room, and activity

---
+++ Library-project/Interfaces/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Library_project.Models;
+
+namespace Library_project.Interfaces
+{
+	public interface IStudentRepository
+	{
+        Task<IEnumerable<students>> GetAll();
+        Task<IEnumerable<students>> GetAllByOverdueFeesAsync();
+
+        //CRUD
+        bool Add(students student);
+        bool Update(students student);
+        bool Delete(students student);
+        bool Save();
+    }
+}
+
