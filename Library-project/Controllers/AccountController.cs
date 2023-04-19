ea6d19b04c6269e0f9290b600db9e791de41bd1d
Elijah Ochoa <elijah.8888@yahoo.com>
2023-04-18 22:41:28 -0600

    fixed auth0

--- Library-project/Controllers/AccountController.cs
+++ Library-project/Controllers/AccountController.cs
@@ -5,37 +5,37 @@ using System.Security.Claims;
 
 namespace Library_project.Controllers
 {
-    public class AccountController: Controller
+    public class AccountController : Controller
     {
         IConfiguration _config;
         public AccountController(IConfiguration config)
         {
-            _config = config; 
+            _config = config;
         }
         public IActionResult StudentRegistration()
         {
-            RegisterStudentVM newStudent=new RegisterStudentVM();
+            RegisterStudentVM newStudent = new RegisterStudentVM();
+            newStudent.email= User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
             return View(newStudent);
         }
-
         public IActionResult RegisterStudentLandingPage(RegisterStudentVM student)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 using (var conn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
                 {
                     conn.Open();
                     var insertCommand = "INSERT INTO elijah_student (library_card_number, fname, mname, lname, homeaddress, birthday, email, phonenumber, historianshistorianid, overduefees)";
                     using (var command = new NpgsqlCommand(insertCommand + " VALUES(@library_card_number, @fname, @mname, @lname, @homeaddress, @birthday, @email, @phonenumber,0 ,0)", conn))
-                    { 
-                        command.Parameters.AddWithValue("@library_card_number", "Incomplete");
+                    {
+                        command.Parameters.AddWithValue("@library_card_number",User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
                         command.Parameters.AddWithValue("fname", student.fname);
                         command.Parameters.AddWithValue("mname", student.mname);
                         command.Parameters.AddWithValue("lname", student.lname);
-                        command.Parameters.AddWithValue("homeaddress",      student.homeaddress);
+                        command.Parameters.AddWithValue("homeaddress", student.homeaddress);
                         command.Parameters.AddWithValue("birthday", student.birthday);
                         command.Parameters.AddWithValue("email", student.email);
-                        command.Parameters.AddWithValue("phonenumber", student.email);
+                        command.Parameters.AddWithValue("phonenumber", student.phonenumber);
 
                         command.ExecuteNonQuery();
                     }
@@ -50,16 +50,27 @@ namespace Library_project.Controllers
             return Redirect("Login");
 
         }
-        public async Task Login(RegisterStudentVM newStudent, string returnUrl = "Account/PostLoginCheck")
+        public async Task Register(RegisterStudentVM newStudent, string returnUrl = "Account/StudentRegistration")
         {
             var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
                 .WithRedirectUri(returnUrl)
                 .Build();
 
-            
+
 
             await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-            
+
+        }
+        public async Task Login(RegisterStudentVM newStudent, string returnUrl = "/Explore")
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+
         }
 
         public IActionResult PostLoginCheck()
@@ -68,17 +79,17 @@ namespace Library_project.Controllers
         }
         public void CompleteRegistration()
         {
-            string id="";
+            string id = "";
             string localEmail = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
             using (var conn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
             {
                 conn.Open();
-                using (var command = new NpgsqlCommand("SELECT * FROM elijah_student WHERE email='"+ localEmail + "'", conn))
+                using (var command = new NpgsqlCommand("SELECT * FROM elijah_student WHERE email='" + localEmail + "'", conn))
                 {
                     var reader = command.ExecuteReader();
                     if (reader.Read())
                     {
-                        id=reader.GetString(0);
+                        id = reader.GetString(0);
                     }
                     else
                     {
@@ -87,7 +98,7 @@ namespace Library_project.Controllers
                 }
 
             }
-            if(id=="Incomplete")
+            if (id == "Incomplete")
             {
                 using (var subConn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
                 {
@@ -101,18 +112,18 @@ namespace Library_project.Controllers
                     }
                 }
             }
-           
+
             Redirect("Home/Index");
-            
+
 
         }
         public void updateLibraryCard(string card)
         {
-            using(var conn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
+            using (var conn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
             {
                 conn.Open();
                 var updateCommand = "UPDATE elijah_student SET library_card_number=@lc WHERE email=@email";
-                using(var command = new NpgsqlCommand(updateCommand,conn))
+                using (var command = new NpgsqlCommand(updateCommand, conn))
                 {
                     command.Parameters.AddWithValue("lc", card);
                     command.Parameters.AddWithValue("emal", User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value);
@@ -137,7 +148,7 @@ namespace Library_project.Controllers
         {
             string localEmail = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
 
-            ViewStudentVM myStudent = new ViewStudentVM();  
+            ViewStudentVM myStudent = new ViewStudentVM();
 
             using (var conn = new NpgsqlConnection(_config.GetConnectionString("local_lib")))
             {
@@ -145,13 +156,13 @@ namespace Library_project.Controllers
                 using (var command = new NpgsqlCommand("SELECT * FROM elijah_student WHERE email='" + localEmail + "'", conn))
                 {
                     var reader = command.ExecuteReader();
-                    while(reader.Read())
+                    while (reader.Read())
                     {
                         myStudent.library_card_number = reader.GetString(0);
                         myStudent.fname = reader.GetString(1);
                         myStudent.mname = reader.GetString(2);
                         myStudent.phonenumber = reader.GetString(4);
-                        if(reader.GetInt16(5)!=null)
+                        if (reader.GetInt16(5) != null)
                         {
                             myStudent.historianid = reader.GetInt16(5);
                         }
@@ -159,7 +170,8 @@ namespace Library_project.Controllers
                         myStudent.lname = reader.GetString(7);
                         myStudent.homeaddress = reader.GetString(8);
                         myStudent.birthday = reader.GetFieldValue<DateOnly>(9).ToString("yyyy-MM-dd");
-                        myStudent.email = User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+                        myStudent.email=reader.GetString(3);
+                        
 
                     }
                 }
@@ -170,7 +182,6 @@ namespace Library_project.Controllers
 
 
     }
-        
+
 
 }
-
