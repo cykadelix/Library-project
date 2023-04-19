266cba6a410f97113ccd719251736e97107a7138
Elijah Ochoa <elijah.8888@yahoo.com>
2023-04-19 13:33:48 -0600

    fix conficts

--- Library-project/Controllers/CheckoutController.cs
+++ Library-project/Controllers/CheckoutController.cs
@@ -1,15 +1,19 @@
 ﻿using Library_project.ViewModels.Checkout;
+using Library_project.Data.Enums;
+using Library_project.Data.Objects;
 using Library_project.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Npgsql;
+
 namespace Library_project.Controllers
 {
-    public class CheckoutController : Controller
+    public class checkoutController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _config;
 
-        public CheckoutController(ILogger<HomeController> logger, IConfiguration config)
+        public checkoutController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
@@ -19,164 +23,81 @@ namespace Library_project.Controllers
             return View();
         }
 
-        public IActionResult CreateCheckout()
+        public IActionResult createCheckout()
         {
-            var newCheckout = new CreateCheckoutViewModel();
+            var newCheckout = new CheckoutViewModel();
             return View(newCheckout);
         }
 
         [HttpPost]
-        public string CreateCheckout(CreateCheckoutViewModel newCheckout)
+
+
+        public async Task<IActionResult> CreateCheckoutLandingPage(CheckoutViewModel newCheckout)
         {
+            await using NpgsqlConnection conn = new NpgsqlConnection(_config.GetConnectionString("local_lib"));
+            var newCheckout2 = new CreateCheckoutViewModel();
             if (ModelState.IsValid)
             {
-                using NpgsqlConnection conn = new NpgsqlConnection(_config.GetConnectionString("local_lib"));
 
-                conn.Open();
-
+                await conn.OpenAsync();
                 try
                 {
-                    using var cmd = new NpgsqlCommand("INSERT INTO checkouts (studentid, mediaid, checkoutdate,returndate, checkoutid, returned) VALUES (@studentid, @mediaid, current_timestamp, current_timestamp + INTERVAL '1 month', DEFAULT, DEFAULT)", conn)
+                    await using var cmd = new NpgsqlCommand("INSERT INTO checkouts (studentid, mediaid, checkoutdate,returndate, checkoutid, returned) VALUES (@studentid, @mediaid, current_timestamp, current_timestamp + INTERVAL '1 month', DEFAULT, DEFAULT)", conn)
                     {
                         Parameters =
                         {
-                            new("studentid", newCheckout.studentid),
+                            new("studentid",newCheckout.studentid),
                             new("mediaid", newCheckout.mediaid)
                         }
                     };
-                    using var reader = cmd.ExecuteReader();
+                    await using var reader = await cmd.ExecuteReaderAsync();
                 }
-                catch (Exception ex)
+                catch(Exception ex)
                 {
-                    return ex.Message;
+
+                    TempData["AlertMessage"] = ex.Message;
+                    return View(newCheckout2);
                 }
-
-            }
-            return "";
-        }
-
-        [HttpGet]
-        public IActionResult CheckoutItem(int mediaId)
-        {
-            if (ModelState.IsValid)
+            }   
+            else
             {
-                using NpgsqlConnection conn = new NpgsqlConnection(_config.GetConnectionString("local_lib"));
-                conn.Open();
-                string command = "UPDATE checkouts SET returned = true, returned_date = CURRENT_TIMESTAMP(0)" +
-                    "WHERE mediaid=@m1";
-                using var cmd = new NpgsqlCommand(command, conn)
-                {
-                    Parameters =
-                        {
-                            new("m1", mediaId)
-                        }
-                };
-                using var reader = cmd.ExecuteReader();
+                newCheckout.studentid = -1;
             }
-            return View("~/Views/Student/StudentCheckouts.cshtml");
+			
+			return View(newCheckout2);
         }
 
-        public List<checkouts>? CheckoutList()
-        {
+		public async Task<IActionResult> CheckoutList()
+		{
 
-            var dataSourceBuilder = new NpgsqlDataSourceBuilder(_config.GetConnectionString("local_lib"));
+			var dataSourceBuilder = new NpgsqlDataSourceBuilder(_config.GetConnectionString("local_lib"));
 
-            using var dataSource = dataSourceBuilder.Build();
-            using var command = dataSource.CreateCommand("SELECT * FROM checkouts");
-            using var reader = command.ExecuteReader();
+			await using var dataSource = dataSourceBuilder.Build();
+			await using var command = dataSource.CreateCommand("SELECT * FROM checkouts");
+			await using var reader = await command.ExecuteReaderAsync();
 
-            var checkoutList = new CheckoutListViewModel();
-            var LocalList = new List<checkouts>();
-            while (reader.Read())
-            {
-                LocalList.Add(new checkouts()
-                {
-                    checkoutid = (int)reader["checkoutid"],
-                    studentid = (int)reader["studentid"],
-                    mediaid = (int)reader["mediaid"],
-                    checkoutdate = (DateTime)reader["checkoutdate"],
-                    returndate = (DateTime)reader["returndate"],
-                    returned = (bool)reader["returned"],
-                    returned_date = (reader.IsDBNull(6) ? "" : reader.GetDateTime(6).ToString())
-                });
+			var checkoutList = new CheckoutListViewModel();
+			var LocalList = new List<checkouts>();
+			while (await reader.ReadAsync())
+			{
+				LocalList.Add(new checkouts()
+				{
+					checkoutid = (int)reader["checkoutid"],
+					studentid = (int)reader["studentid"] ,
+					mediaid = (int)reader["mediaid"],
+					checkoutdate = (DateTime)reader["checkoutdate"],
+					returndate = (DateTime)reader["returndate"]
+				
+					//publicdate = reader.GetFieldValue<DateOnly>(3),
+				
+				});
 
-                checkoutList.allCheckouts = LocalList;
-            }
-            if (LocalList.Count == 0) return null;
-
-            return LocalList;
-        }
-
-        public List<StudentCheckoutsViewModel>? StudentCheckoutList(int studentid)
-        {
-
-            var dataSourceBuilder = new NpgsqlDataSourceBuilder(_config.GetConnectionString("local_lib"));
-
-            using var dataSource = dataSourceBuilder.Build();
-            string comm = "SELECT null as brand, null as serialnumber, title, checkoutdate, returndate, returned, returned_date, 'Book' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, books " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = books.mediaid) UNION " +
-
-                "SELECT brand, serialnumber, null as title, checkoutdate, returndate, returned, returned_date, 'Camera' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, cameras " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = cameras.mediaid) UNION " +
-
-                "SELECT brand, serialnumber, null as title, checkoutdate, returndate, returned, returned_date, 'Computer' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, computers " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = computers.mediaid) UNION " +
-
-                "SELECT null as brand, null as serialnumber, title, checkoutdate, returndate, returned, returned_date, 'Audiobook' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, audiobooks " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = audiobooks.mediaid) UNION " +
-
-                "SELECT null as brand, null as serialnumber, title, checkoutdate, returndate, returned, returned_date, 'Journal' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, journals " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = journals.mediaid) UNION " +
-
-                "SELECT null as brand, null as serialnumber, title, checkoutdate, returndate, returned, returned_date, 'Movie' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, movies " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = movies.mediaid) UNION " +
-
-                "SELECT brand, serialnumber, null as title, checkoutdate, returndate, returned, returned_date, 'Projector' as mediatype, checkouts.mediaid " +
-                "FROM checkouts, projectors " +
-                "WHERE ( checkouts.studentid = '" + studentid + "' AND checkouts.mediaid = projectors.mediaid)";
-
-            using var command = dataSource.CreateCommand(comm);
-            using var reader = command.ExecuteReader();
-
-            var LocalList = new List<StudentCheckoutsViewModel>();
-            while (reader.Read())
-            {
-                LocalList.Add(new StudentCheckoutsViewModel()
-                {
-                    Brand = reader.IsDBNull(0) ? "" : reader.GetString(0),
-                    SerialNo = reader.IsDBNull(1) ? "" : reader.GetString(1),
-                    Title = reader.IsDBNull(2) ? "" : reader.GetString(2),
-                    CheckoutDate = reader.GetDateTime(3).ToString("MM-dd-yyyy HH:mm"),
-                    ReturnDate = reader.GetDateTime(4).ToString("MM-dd-yyyy HH:mm"),
-                    Returned = reader.IsDBNull(5) ? false : reader.GetBoolean(5),
-                    ReturnedDate = reader.IsDBNull(6) ? "" : reader.GetDateTime(6).ToString(),
-                    MediaType = reader.GetString(7),
-                    mediaId = reader.GetInt32(8),
-                });
-            }
-            if (LocalList.Count == 0) return null;
-
-            return LocalList;
-        }
-
-        [HttpGet]
-        public IActionResult GetStudentsCheckoutList(int studentid)
-        {
-            return Json(StudentCheckoutList(studentid));
-        }
-
-        [HttpGet]
-        public IActionResult GetCheckoutList()
-        {
-            return Json(CheckoutList());
-        }
+				checkoutList.allCheckouts = LocalList;
+			}
 
 
-    }
+			return View(checkoutList);
+
+		}
+	}
 }
