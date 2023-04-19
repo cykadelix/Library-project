cf87c579be7134867e1a81c38ff9a35133c89d10
rphong <richard.phong424@gmail.com>
2023-04-19 17:38:56 -0500

    working report

---
+++ Library-project/wwwroot/js/checkoutReport.cs
@@ -0,0 +1,16 @@
+﻿﻿$("#report-type").on("change", function() {
+    let selectedType = $(this).val();
+
+    if (selectedType == "Checkouts")
+    {
+        window.location.href = '../Reports/CheckoutsByDateIndex';
+    }
+    else if (selectedType == "Ratings")
+    {
+        window.location.href = '../Reports/AverageRatingIndex';
+    }
+    else if (selectedType == "Students")
+    {
+        window.location.href = '../Reports/StudentsByDateIndex';
+    }
+});
