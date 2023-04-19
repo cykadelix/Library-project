2482e93fea531402a6d001eb89cd8b10c4131a91
Emmanuel Chavez <chavemm15@gmail.comchavemm@Emmanuels-Air ~ % git config --global user.email chavemm15@gmailcomgit config --list>
2023-04-19 16:49:08 -0500

    Readded student controller and added reports

---
+++ Library-project/ViewModels/Reports/overduefeesReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Library_project.ViewModels.Reports
+{
+	public class overduefeesReportViewModel
+	{
+        public string? fname { get; set; }
+        public string? lname { get; set; }
+		public int overduefees { get; set; }
+		
+	}
+}
+
