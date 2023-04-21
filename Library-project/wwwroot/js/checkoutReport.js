6dbdceb8296722a0f19f7ca7c76ffb027d95512e
rphong <richard.phong424@gmail.com>
2023-04-20 19:42:57 -0500

    Fixed report selector not working.

--- Library-project/wwwroot/js/checkoutReport.js
+++ Library-project/wwwroot/js/checkoutReport.js
@@ -1,16 +1,13 @@
-﻿﻿$("#report-type").on("change", function() {
+﻿$("#report-type").on("change", function () {
     let selectedType = $(this).val();
 
-    if (selectedType == "Checkouts")
-    {
+    if (selectedType == "Checkouts") {
         window.location.href = '../Reports/CheckoutsByDateIndex';
     }
-    else if (selectedType == "Ratings")
-    {
+    else if (selectedType == "Ratings") {
         window.location.href = '../Reports/AverageRatingIndex';
     }
-    else if (selectedType == "Students")
-    {
+    else if (selectedType == "Students") {
         window.location.href = '../Reports/StudentsByDateIndex';
     }
 });
